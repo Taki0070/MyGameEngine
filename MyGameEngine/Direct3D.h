09c2aa0b35cc1b1e40463138bb5083947d1c3311
taki <230531@jc-21.jp>
2024-05-21 14:24:07 +0900

    まとめた

--- MyGameEngine/Direct3D.h
+++ MyGameEngine/Direct3D.h
@@ -1,5 +1,25 @@
 #pragma once
-class Direct3D
+#include <d3d11.h>
+
+//リンカ
+#pragma comment(lib, "d3d11.lib")
+
+
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+
+namespace Direct3D
 {
+	//初期化
+	void Initialize(int winW, int winH, HWND hWnd);
+
+	//描画開始
+	void BeginDraw();
+
+	//描画終了
+	void EndDraw();
+
+	//解放
+	void Release();
 };
 
